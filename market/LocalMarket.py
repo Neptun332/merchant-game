040e009536cdd05997cb20405605e71d2f7d9d01
Neptun332 <patrykbednarz8@gmail.com>
2023-05-13 15:35:11 +0200

    refactored demand and utility

--- market/LocalMarket.py
+++ market/LocalMarket.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from typing import Dict
 
 import numpy
@@ -13,7 +12,7 @@ class LocalMarket:
         self.resources_map = resources_map
 
     def update(self, demand: Dict[ResourceName, int]):
-        utility = self._get_utility(demand)
+        utility = self._get_utility_v2()
         for resource_name, resource in self.resources_map.items():
             resource.update(demand.get(resource_name, 0), utility.get(resource_name, 0))
 
@@ -23,7 +22,17 @@ class LocalMarket:
 
     def _get_utility(self, demand: Dict[ResourceName, int]) -> Dict[ResourceName, int]:
         return {
-            resource_name: 50*numpy.log2( max(0, float(self.resources_map.get(resource_name, 0).units + 2)))
+            resource_name: 12*numpy.log2(max(0, float(self.resources_map.get(resource_name, 0).units) +2))
             for resource_name, units in demand.items()
         }
 
+    def _get_utility_v2(self) -> Dict[ResourceName, int]:
+        return {
+            resource_name: resource.units
+            for resource_name, resource in self.resources_map.items()
+        }
+
+
+
+
+# Utility is something you cna count, lets calculate demand based on missing resources (magic way)
--- market/Resource.py
+++ market/Resource.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass, field
 from decimal import Decimal
 from typing import List
 
+import numpy
+
 from Factor import Factor
 from market.ResourceName import ResourceName
 
@@ -19,8 +21,11 @@ class Resource:
     def remove_units(self, units: int):
         self.units -= units
 
-    def update(self, demand: int, utility: int, demand_change_factor: Factor = Factor(Decimal("0.5"))) -> Decimal:
-        price_per_unit = demand-utility * float(demand_change_factor.value)
+    def update(self, demand: int, utility: int, demand_change_factor: Factor = Factor(Decimal("1"))) -> Decimal:
+        price_per_unit = self._price_per_unit_based_on_demand(demand, utility) * float(demand_change_factor.value)
         self.price_per_unit = Decimal(max(0, price_per_unit))
         self.history_of_price.append(self.price_per_unit)
         return self.price_per_unit
+
+    def _price_per_unit_based_on_demand(self, demand: int, utility: int, ) -> float:
+        return 1 * numpy.log2(max(0, float(demand - utility) - 100))
